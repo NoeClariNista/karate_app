2c03cea2a3568c3b264c8b9c856ed548ed370224
NoeClariNista <chispa1992@gmail.com>
2026-05-17 19:03:48 +0100

    feat(types): Se añade el archivo types donde podemos ir añadiendo estructuras y seguridad a los datos y a la navegacion.

---
+++ karate-live/src/types.ts
@@ -0,0 +1,14 @@
+export type Match = {
+  id: number
+  tatami: number | string
+  red: string
+  blue: string
+  winner?: string | null
+}
+
+export type RootStackParamList = {
+  Inicio: undefined;
+  Evento: undefined;
+  Admin: undefined;
+  Scanner: undefined;
+};
